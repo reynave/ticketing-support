ebbb269c266005b98c94e8009af6b0e47e0f1526
felix <felix.renaidl@gmaio.com>
2026-08-10 17:04:29 +0700

    update env

--- public/env/env.js
+++ public/env/env.js
@@ -1,4 +1,5 @@
 const apiBaseUrl = 'https://crm.co.id/api-dev/';
+const production = false;
 
 /**
  * This file is used to define environment variables for the application.
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 declare var apiBaseUrl: string;
-
+declare var production: boolean;
 export const environment = {
 	apiBaseUrl: apiBaseUrl,
-	production: true,
+	production: production || true,
 	//apiBaseUrl: 'https://crm.co.id/api',
 	// apiBaseUrl: 'https://crm.co.id/api-dev/'
 };
